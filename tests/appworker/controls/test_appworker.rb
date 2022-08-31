740c109cc4324663e71d1c572af5143417ffacd1
Brais Cantorna <bctorreira@tutanota.com>
2022-08-31 16:28:52 +0000

    First Inspec testing commit

---
+++ tests/appworker/controls/test_appworker.rb
@@ -0,0 +1,34 @@
+# Test the "appworker" role.
+
+# Get package list
+my_packages = yaml(content: inspec.profile.file('packages.yml')).params
+
+# Get user list
+my_users = yaml(content: inspec.profile.file('users.yml')).params
+
+# Get file list
+my_files = yaml(content: inspec.profile.file('files.yml')).params
+
+# Check for installed packages
+my_packages.each do |p|
+    describe package(p) do
+        it { should be_installed }
+        it { should be_latest }
+    end
+end
+
+# Check for users
+my_users.each do |u|
+    describe user(u) do
+        it { should exist }
+    end
+end
+
+# Check for files
+my_files.each do |f|
+    describe file(f) do
+        it { should exist }
+        it { should be_file }
+        its('size') { should_not be 0 }
+    end
+end
